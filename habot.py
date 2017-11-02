e81158200ea3688d8395cdbbe8b9ab4edfea7fac
harry.ha(하현수) <harry.ha@kakaocorp.com>
2017-11-02 13:25:46 +0900

    1

---
+++ habot.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root():
+    return 'root!!!!!!!!!!!!'
+
+
+@app.route('/test')
+def root():
+    return 'test'
+
+
+if __name__ == '__main__':
+    app.run()
